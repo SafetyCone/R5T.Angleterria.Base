1268e45828579fb311841a776d1a921ea7ef7ecb
davidcoats <david@getsafetycone.com>
2022-04-27 15:24:57 -0700

    Interim changes.

---
+++ source/R5T.Angleterria.Base/Code/Services/Definitions/IVisualStudioStringlyTypedPathPartsOperator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using R5T.T0064;
+
+
+namespace R5T.Angleterria
+{
+    [ServiceDefinitionMarker]
+    public interface IVisualStudioStringlyTypedPathPartsOperator : IServiceDefinition
+    {
+        string GetProjectDirectoryNameForProjectName(string projectName);
+    }
+}
